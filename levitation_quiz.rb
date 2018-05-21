364fa6306a7297a897afbc8e2a9376ebbc58b11d
iwilliams83 <indirajamurath@gmail.com>
2018-05-21 14:53:12 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,9 @@
 
 def levitation_quiz
 	#your code here
+  phrase = gets.chomp until phrase = "Wingardium Leviosa"
+  puts "You passed the quiz!"
+  #while phrase != "Wingardium Leviosa"
+  #  phrase = gets.chomp
+  #  if phrase = "Win"
 end
-
-
